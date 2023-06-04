614eff381ccf777c85f6750f3fae652eda014802
raynertjx <rayner742@gmail.com>
2023-06-04 23:14:00 +0800

    Added theme handling and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import { useState } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Contact from "./components/Contact";
-import Experience from "./components/Experience";
-import About from "./components/About";
-import Hello from "./components/Hello";
-import Projects from "./components/Projects";
+import './App.css';
+import Header from './components/Header';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
+import About from './components/About';
+import Hello from './components/Hello';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
   return (
-    <main className="mx-auto max-w-[130ch]">
-      <Header />
-      <Hello />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
+    <main className='transition dark:bg-[#2d2d2d] dark:text-[#dfdfdf]'>
+      <div className='mx-auto max-w-[120ch]'>
+        <Header />
+        <Hello />
+        <About />
+        <Experience />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
     </main>
   );
 }

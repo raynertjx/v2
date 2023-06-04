adeea19f96b88a2c6e83bfcc83589ec2aea08e71
raynertjx <rayner742@gmail.com>
2023-06-04 23:15:18 +0800

    Added project image

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,19 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import type { FC } from 'react';
-import Python from './public/icons/python.svg';
 
 interface ProjectsProps {}
 
-const Projects: FC<ProjectsProps> = ({}) => {
+const Projects: FC<ProjectsProps> = () => {
   return (
     <section className='min-h-[100vh]' id='projects'>
       <h1 className='mb-12'>
-        <span className='text-5xl font-medium mb-12 text-transparent bg-clip-text bg-gradient-to-l from-orange-600 to-amber-500'>
-          Projects
+        <span className='text-5xl font-medium mb-12 text-accent-orange'>
+          projects.
         </span>
       </h1>
-      {/* <img src={Python} alt='xd' /> */}
+      <div>
+        <img src='public/project_forum.jpeg' alt='The Soccat Forum' width={400}/>
+      </div>
     </section>
   );
 };

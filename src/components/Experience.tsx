1b36045d63cc7bd86ee34e945eeb3eb10af23b23
raynertjx <rayner742@gmail.com>
2023-06-04 23:15:43 +0800

    Added underline hover animation

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,24 +1,28 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import type { FC } from 'react';
-import { TbCaretRight } from 'react-icons/tb';
-import Card from './Card.astro';
 
 interface ExperienceProps {}
 
-const Experience: FC<ExperienceProps> = ({}) => {
+const Experience: FC<ExperienceProps> = () => {
   return (
     <section className='min-h-[100vh]' id='experience'>
-      <span className='inline-block text-5xl pb-6'>💼</span>
+      {/* <span className='inline-block text-5xl pb-6'>💼</span> */}
       <h1 className='mb-12'>
-        <span className='text-5xl font-medium mb-12 text-transparent bg-clip-text bg-gradient-to-l from-orange-600 to-amber-500'>
-          Experience
+        <span className='text-5xl font-medium mb-12 text-accent-orange'>
+          experience.
         </span>
       </h1>
       <div className='flex flex-col gap-4'>
-        <div className='text-gray-700 text-3xl font-medium'>
-          May 2023 - Present
-        </div>
+        <div className='text-3xl font-medium'>May 2023 - Present</div>
         <div className='text-4xl font-medium'>
-          Software Engineer Intern @ KABAM Robotics
+          Software Engineer Intern @{' '}
+          <a
+            className='before:scale-x-100 before:origin-left relative before:w-full before:h-0.5 before:transition-transform before:duration-300 before:bg-black dark:before:bg-[#dfdfdf] before:absolute before:left-0 before:bottom-0 hover:before:scale-x-0 hover:before:origin-right'
+            href='https://kabam.ai/'
+            target='_blank'
+          >
+            KABAM Robotics
+          </a>
         </div>
         <ul className='text-2xl flex flex-col gap-3 w-4/5'>
           <li>
@@ -31,7 +35,8 @@ const Experience: FC<ExperienceProps> = ({}) => {
           </li>
           <li>
             &#62; &ensp;Collaborated with cross-functional teams to ensure
-            smooth integration and continuous improvement of the web app.
+            smooth integration and continuous improvement of the web
+            application.
           </li>
           <li>
             &#62; &ensp;Implemented RESTful APIs to facilitate efficient data
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -10,16 +10,26 @@ const Hello: FC<HelloProps> = () => {
         <span className='inline-block animate-wave text-5xl pb-6'>👋🏼</span>
         <div className='text-3xl'>Hi there! My name is</div>
         <div className='-translate-x-1 text-8xl'>
-          <span className='text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-amber-500 font-bold'>
+          <span
+            className={`text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-amber-500 font-bold`}
+          >
             Rayner Toh.
           </span>
         </div>
       </div>
-      <div className='text-5xl font-medium text-gray-700'>
+      <div className='text-5xl font-medium'>
         I like to build things for the web.
       </div>
-      <div className='text-2xl text-gray-600'>
-        Currently working as a Software Engineer Intern at KABAM Robotics.
+      <div className='text-2xl'>
+        Currently working as a Software Engineer Intern at{' '}
+        <a
+          className='before:scale-x-100 before:origin-left relative before:w-full before:h-0.5 before:transition-transform before:duration-300 before:bg-black dark:before:bg-[#dfdfdf] before:absolute before:left-0 before:bottom-0 hover:before:scale-x-0 hover:before:origin-right'
+          href='https://kabam.ai/'
+          target='_blank'
+        >
+          KABAM Robotics
+        </a>
+        .
       </div>
     </div>
   );

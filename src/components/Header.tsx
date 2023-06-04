ebf644f88bc1808c40914a7c38ea6114b867da5b
raynertjx <rayner742@gmail.com>
2023-06-04 23:15:10 +0800

    Added dark mode toggle button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,44 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
-import type { FC } from "react";
+import type { FC } from 'react';
+import { useState } from 'react';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
-  // const [isDarkMode, setDarkMode] = useState(false);
+  const [isDarkMode, setDarkMode] = useState(
+    localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches),
+  );
 
-  // const toggleDarkMode = () => {
-  //   setDarkMode(true);
-  // };
+  const toggleDarkMode = (checked: boolean) => {
+    setDarkMode(checked);
+    if (localStorage.theme === 'light') {
+      localStorage.theme = 'dark';
+      document.documentElement.classList.add('dark');
+    } else {
+      localStorage.theme = 'light';
+      document.documentElement.classList.remove('dark');
+    }
+  };
 
   return (
-    <header className="flex justify-between pt-2">
-      <h1 className="text-2xl font-semibold">Rayner.</h1>
-      <div className="flex gap-16 text-xl font-medium">
-        <a href="#about">About</a>
-        <a href="#experience">Experience</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+    <header className='flex justify-between pt-8'>
+      <h1 className='text-2xl font-semibold'><a href="/">Rayner.</a></h1>
+      <div className='flex gap-14 text-xl font-medium'>
+        <a href='#about'>about</a>
+        <a href='#experience'>experience</a>
+        <a href='#projects'>projects</a>
+        <a href='#contact'>contact</a>
+        <DarkModeSwitch
+          style={{ marginBottom: '2rem' }}
+          checked={isDarkMode}
+          onChange={toggleDarkMode}
+          size={30}
+          sunColor='rgb(249, 115, 22)'
+          moonColor='rgb(87, 156, 216)'
+        />
       </div>
     </header>
   );
